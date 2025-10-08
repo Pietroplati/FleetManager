9a87257a623f6ccb4f0078fc6b30aa3941a11158
Pietroplati <p.plati@studenti.unibg.it>
2025-10-08 19:14:06 +0200

    Create calculator.java

---
+++ codice/FleetManager/src/main/java/PlaBas/FleetManager/calculator.java
@@ -0,0 +1,13 @@
+package PlaBas.FleetManager;
+
+public class calculator {
+
+	
+	public float somma(float a, float b) {
+		return a+b;
+	}
+	
+	public float sottrazione(float a, float b) {
+		return a-b;
+	}
+}
